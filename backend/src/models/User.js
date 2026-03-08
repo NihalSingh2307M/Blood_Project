3d75036f0b803e0f5b4add619ded49f4b49cae80
NihalSingh <nihalsingh2307n@gmail.com>
2026-03-08 22:33:26 +0530

    src->module->User.js:status:done

---
+++ backend/src/models/User.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    
+    name:{
+        type:String,
+        required:true
+    },
+
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+
+    password:{
+        type:String,
+        required:true
+    },
+
+    bloodGroup:{
+        type:String,
+        required:true
+    },
+    
+    age:{
+        type:Number
+    },
+
+    isDonor:{
+        type:Boolean,
+        default:false
+    },
+
+    lastDonationDate:{
+        type:Date
+    },
+
+    createdAt:{
+        type:Date,
+        default:Date.now
+    }
+
+})
+
+module.exports = mongoose.model("User",userSchema);
